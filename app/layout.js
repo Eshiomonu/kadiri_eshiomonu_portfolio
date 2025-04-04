2eebd8c316c5690338e3144cebca658de8aee58f
kadirisunday <kadirisunday212@gmail.com>
2025-04-04 13:20:16 +0100

    Header section done

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} ${ovo.className} antialiased`}>
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased overflow-x-hidden leading-8`}
+      >
         {children}
       </body>
     </html>
